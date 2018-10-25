2d392158ce607d6c20af600eae63270ce481d208
전형탁 <andy395023@gmail.com>
2018-10-25 20:58:09 +0900

    외출 스키마, 입력 폼 변경(id값 받아와서 같이 넘겨주도록 구현해야함)

--- models/goout.js
+++ models/goout.js
@@ -2,16 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var gooutSchema = new Schema({
-    student: {
-        name : String,
-        class : String,
-        number : String
-    },
-    goout:{
-        starttime : String,
-        stoptime : String,
-        why : String
-    }
+    id : String,
+    starttime : String,
+    stoptime : String,
+    why : String
 });
 
 module.exports = mongoose.model('goout', gooutSchema);
