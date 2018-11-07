3d4b113fba21829ccab408135d71dc4a4a56e954
전형탁 <andy395023@gmail.com>
2018-11-07 21:20:05 +0900

    StartPage프론트

--- client/src/components/StartPage.vue
+++ client/src/components/StartPage.vue
@@ -1,37 +1,27 @@
 <template>
-<div class="container">
-  <h2>Todo List</h2>
-  <div class="input-group" style="margin-bottom:10px;">
-    <input type="text" class="form-control" 
-      placeholder="할일을 입력하세요" 
-      v-model="name" 
-      v-on:keyup.enter="createTodo(name)">
-    <span class="input-group-btn">
-      <button class="btn btn-default" type="button" 
-      @click="createTodo(name)">추가</button>
-    </span>
-  </div>
-  <ul class="list-group">
-    <li :key="index" class="list-group-item" v-for="(todo, index) in todos">
-      {{todo.name}}
-      <div class="btn-group pull-right"
-        style="font-size: 12px; line-height: 1;">
-        <button type="button" 
-        class="btn-link dropdown-toggle" 
-        data-toggle="dropdown" 
-        aria-haspopup="true" 
-        aria-expanded="false">
-          더보기<span class="caret"></span>
-        </button>
-        <ul class="dropdown-menu">
-          <li>
-            <a href="#" @click="deleteTodo(todo)">삭제</a>
-          </li>
-        </ul>
+  <v-card ref="form" style="margin-left:350px;margin-right:350px;margin-top:80px;">
+    <v-card-text style="text-align:center;">
+      <div class="panel-title" style="margin-top:50px;"><h2>기숙사 관리 프로그램</h2></div>
+      <div class="panel-title">환영합니다!</div>
+      <div class="panel-body" style="margin-bottom:10px;margin-left:20px;margin-right:20px;">
+        <div>
+          <h6 style="text-align:left;">이메일</h6>
+          <input type="text" class="form-control"
+            v-model="email"
+            v-on:keyup.enter="createTodo(name)">
+        </div>
+        <div style="margin-top:5px;">
+          <h6 style="text-align:left;">비밀번호</h6>
+          <input type="password" id="in" class="form-control"
+            placeholder=""
+            v-model="password"
+            v-on:keyup.enter="createTodo(name)">
+        </div>
+          <v-btn block color="secondary" dark style="margin-top:20px;">로그인</v-btn>
       </div>
-    </li>
-  </ul>
-</div>
+      <a href="/signup"><h4>회원가입하기</h4></a>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
