82a46a2c884c4a384f49488df0e7458796800edc
전형탁 <andy395023@gmail.com>
2018-11-12 21:18:49 +0900

    상벌점 목록 출력

--- router/index.js
+++ router/index.js
@@ -90,7 +90,18 @@ router.get('/point', function (req, res) {
             console.log(greenpoint);
             res.render('point', {
                 rp: redpoint,
-                gp: greenpoint
+                gp: greenpoint,
+                formatDate: function(date) {
+                    var d = new Date(date), 
+                    month = '' + (d.getMonth() + 1),
+                    day = '' + d.getDate(),
+                    year = d.getFullYear();
+              
+                    if (month.length < 2) month = '0' + month;
+                    if (day.length < 2) day = '0' + day;
+              
+                    return [year, month, day].join('-');
+                  } 
             });
         });
     });
